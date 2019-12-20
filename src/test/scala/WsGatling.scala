9693be8a1c021fb2555eabe44919d5d1e18f1174
Allencs <allenchensheng@163.com>
2019-12-20 12:54:10 +0800

    _

--- src/test/scala/WsGatling.scala
+++ src/test/scala/WsGatling.scala
@@ -1,25 +1,30 @@
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import scala.concurrent.duration._
+import scala.util.Random
 
 
 class WsGatling extends Simulation {
 
   val Protocol = http
-    .wsBaseUrl("ws://123.207.167.163:9010")
+//    .wsBaseUrl("ws://123.207.167.163:9010")
 //    .wsBaseUrl("ws://10.203.29.217:8085")
+    .wsBaseUrl("ws://localhost:8088")
 
+  val feeder = Iterator.continually(Map("userId" -> (Random.alphanumeric.take(3).mkString)))
 
   val myCheck = ws.checkTextMessage("check")
                   .check(regex(".*").saveAs("res"))
 
   val scenario_ws = scenario("WebSocket")
+    .exec(feed(feeder))
     .exec(ws("Connect")
-        .connect("/ajaxchattest")
-//      .connect("/spring_websocket/websocket")
+        .connect("/websocket/${userId}")
+        .await(0.2 seconds)(myCheck)
+//      .connect("/spring_websocket/websocket/${userId}")
         .onConnected(
           exec(ws("SendMsg")
-          .sendText("HelloWebSocket")
+          .sendText("it's from gatling")
           .await(0.5 seconds)(myCheck))
           )
         )
@@ -27,14 +32,12 @@ class WsGatling extends Simulation {
     .exec{session =>
       println("response: " + session("res").as[String])
     session
-  }.pause(1 minutes)
-
-
+  }.pause(0.5 minutes)
 
     exec(ws("Close").close)
 
   setUp(
-    scenario_ws.inject(atOnceUsers(50))
+    scenario_ws.inject(rampUsers(50) during(25 seconds))
 
   ).protocols(Protocol)
 
